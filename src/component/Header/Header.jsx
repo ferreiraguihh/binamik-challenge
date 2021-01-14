6b02bad673403da48a16606e769812cb4d6b89ca
Guilherme Ferreira <guilherme.ferreira8521@gmail.com>
2021-01-14 20:23:00 -0300

    Header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 import './Header.css';
 import logoBio from '../../assets/img/binamik_logo.png'
+import logoStar from '../../assets/img/star-wars.png'
 import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <div className="header-starwars">
             <a href="https://binamik.com.br/" target="_BLANK">
-            <img src={logoBio}/>
+                <img src={logoBio} />
             </a>
+            <h2>Persons Star Wars</h2>
+            <div className="container-img-starwars">
+                <a href="https://www.starwars.com/" target="_BLANK">
+                    <img src={logoStar} />
+                </a>
+            </div>
         </div>
     );
 }
